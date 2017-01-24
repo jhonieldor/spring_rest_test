222980747c08e682449aef61d83014bb68c3501f
jhonieldor <jhonieldor@gmail.com>
2017-01-23 23:33:49 -0200

    Mais configurações --'

---
+++ src/main/java/com/example/DatabaseConfig.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+/**
+ * Created by JHoNi on 23/01/2017.
+ */
+@Configuration
+public class DatabaseConfig {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return new org.apache.tomcat.jdbc.pool.DataSource();
+    }
+}
